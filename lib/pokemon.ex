868c8029943eb7f86a6687a3f27760febc56aa31
Stan Bakalov <stan.bakalov@podium.com>
2020-06-09 16:19:45 -0600

    finally figured out how to convert nested string key maps. Making sure to save my work so I don't waste another few hours on that. this is one step closer to parsing and caching pokemon results

--- lib/pokemon.ex
+++ lib/pokemon.ex
@@ -20,9 +20,25 @@ defmodule Pokemon do
     case res do
       {:ok, body} ->
         body
+        |> keys_to_atoms()
 
       {:error, err} ->
         Logger.warn(fn -> "[#{__MODULE__}] Pokemon API call failed: #{inspect(err)}" end)
     end
   end
+
+  def create_poke_map(poke_map \\ %{}) do
+    #
+  end
+
+  def keys_to_atoms(string_key_map) when is_map(string_key_map) do
+    for {key, val} <- string_key_map, into: %{}, do: {String.to_atom(key), keys_to_atoms(val)}
+  end
+
+  def keys_to_atoms(string_key_list) when is_list(string_key_list) do
+    string_key_list
+    |> Enum.map(&keys_to_atoms/1)
+  end
+
+  def keys_to_atoms(value), do: value
 end
